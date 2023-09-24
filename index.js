d0c94186e17a7c7f62e308be9f214c3af34c8cdd
AnnVynnychenkoa <avinnichenko93@gmail.com>
2023-09-24 13:59:47 +0300

    hw-01-corrected

--- index.js
+++ index.js
@@ -22,13 +22,3 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 
 const { argv } = yargs(process.argv.slice(2));
 invokeAction(argv);
-
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "05olLMgyVQdWRwgKfg5J6" });
-// invokeAction({
-//   action: "add",
-//   name: "Mango",
-//   email: "mango@gmail.com",
-//   phone: "322-22-22",
-// });
-// invokeAction({ action: "remove", id: "qdggE76Jtbfd9eWJHrssH" });
